fe8c82ab9de55323b10ad1dd6bf3dab3af7b35aa
masindes <maasinde.sylvester@gmail.com>
2024-09-25 17:54:35 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 44;
+let num4 = 10;
+let multiply = num1 * num2 ;
+let random = Math.floor(Math.random() * 100) + 1;
+let mod = num3 % num4 ;
+let max = Math.max(10, 5, 15, 20);
